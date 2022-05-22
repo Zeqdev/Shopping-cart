324b9b0f05977e0cf8d7f0292cff024e97fc30ed
Zeq <Ezeequiellopez97@gmail.com>
2022-05-22 19:58:49 -0300

    LocalStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Home } from '../pages/Home';
 import { ShoppingCart } from '../pages/ShoppingCart';
 import styles from './App.module.css';
 
 export function App() {
 	const [cart, setCart] = useState([]);
+
+	useEffect(() => {
+		const getLocalCart = () => {
+			if (localStorage.getItem('cart') === null) {
+				localStorage.setItem('cart', JSON.stringify([]));
+			} else {
+				let localCart = JSON.parse(localStorage.getItem('cart'));
+				setCart(localCart);
+			}
+		};
+		getLocalCart();
+	}, []);
+
+	useEffect(() => {
+		const saveLocalCart = () => {
+			localStorage.setItem('cart', JSON.stringify(cart));
+		};
+		saveLocalCart();
+	}, [cart]);
 
 	return (
 		<BrowserRouter>
